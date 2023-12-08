778c3cceb8c1cd77e4e1c55f5f7fbeca09808cc8
Bin Han <39442036+BeanHam@users.noreply.github.com>
2023-12-07 20:23:20 -0800

    Update pretrain_evaluation.py

--- tasks/pretrain_evaluation.py
+++ tasks/pretrain_evaluation.py
@@ -13,22 +13,6 @@ from evaluate_summarization import evaluate_hf_model
 import transformers
 import torch
 
-MODEL_CHAT_TOKENS = {
-    'openai': '',
-    'mistral': '<s>[INST] ',
-    'llama-2': '<s>[INST] <<SYS>>\nYou are a helpful assistant.\n<</SYS>>\n\n',
-    'falcon': 'A helpful assistant.\nUser: ',
-    'opt-finetune': '',
-}
-
-MODEL_END_PROMPTS = {
-    'openai': ' ',
-    'mistral': '[/INST] ',
-    'llama-2': '[/INST] ',
-    'falcon': '\nAssistant: ',
-    'opt-finetune': ' ',
-}
-
 MODEL_SUFFIXES = {
     'openai': '',
     'mistral': '</s>',
@@ -47,16 +31,16 @@ def main():
     #-------------------    
     parser = argparse.ArgumentParser()
     parser.add_argument('--model_id', type=str, default='True')
-    parser.add_argument('--dataset', type=str, default='beanham/wikiqa')
-    parser.add_argument('--input_column', type=str, default='Question')
-    parser.add_argument('--target_column', type=str, default='Sentence')
+    parser.add_argument('--dataset', type=str, default='beanham/medsum')
+    parser.add_argument('--input_column', type=str, default='dialogue')
+    parser.add_argument('--target_column', type=str, default='section_text')
     parser.add_argument('--suffix', type=str, default='</s>', help='The suffix to add to the end of the input and target text.')
-    parser.add_argument('--chat_format', type=str, default='mistral', help='Whether to use the default prompts for a model')
+    parser.add_argument('--use_model_prompt_defaults', type=str, default='mistral', help='Whether to use the default prompts for a model')
     parser.add_argument('--device', type=str, default='cuda:0', help='The device to mount the model on.')
     parser.add_argument('--shot', type=str, default='0', help='The device to mount the model on.')
     args = parser.parse_args()
-    start_prompt = 'Please summarize the following conversation:\n\n'
-    end_prompt = '\n\nBegin summary: '
+    if args.use_model_prompt_defaults:
+        args.suffix = MODEL_SUFFIXES[args.use_model_prompt_defaults]
     
     #-------------------
     # load data
@@ -65,24 +49,19 @@ def main():
     train_data = load_dataset(args.dataset, split='train')
     validation_data = load_dataset(args.dataset, split='validation')
     test_data = load_dataset(args.dataset, split='test')
-    oneshot = f"\n\n## Here is an example:\n{train_data[0]['dialogue']}.\n\nSummary:\n{train_data[0]['section_text']}"
-    twoshot = f"\n\n## Here is another example:\n{train_data[1]['dialogue']}.\n\nSummary:\n{train_data[1]['section_text']}"
-    threeshot = f"\n\n## Here is another example:\n{train_data[2]['dialogue']}.\n\nSummary:\n{train_data[2]['section_text']}"
-    transition = "\n\nNow please summarize the following conversation:\n\n"
     
-    if args.use_model_prompt_defaults:
-
-        args.start_prompt = MODEL_CHAT_TOKENS[args.use_model_prompt_defaults] + args.start_prompt
-        args.end_prompt = args.end_prompt + MODEL_END_PROMPTS[args.use_model_prompt_defaults]
-        args.suffix = MODEL_SUFFIXES[args.use_model_prompt_defaults]    
-
-    # oneshot
-    if int(args.shot) == 1:        
-        args.start_prompt = args.start_prompt+oneshot+transition
-    if int(args.shot) == 2:        
-        args.start_prompt = args.start_prompt+oneshot+twoshot+transition
-    if int(args.shot) == 3:
-        args.start_prompt = args.start_prompt+oneshot+twoshot+threeshot+transition
+    system_message = """You are a helpful medical assistant! Please help me summarize dialogues between doctors and patients. I will provide you with each dialogue, as well as the topic for that dialogue. """
+    transaction = """\n\nPlease summarize the following dialogue."""
+    example_1_question = f"""\n\nExample 1:\n\n## Dialogue:\n{train_data[0]['dialogue']}\n\n## Topic:\n{train_data[0]['section_header']}\n\n## Summary:"""
+    example_1_response = f"""{train_data[0]['section_text']}"""
+    example_2_question = f"""Here is another example example:\n\nExample 2:\n\n## Dialogue:\n{train_data[1]['dialogue']}\n\n## Topic:\n{train_data[1]['section_header']}\n\n## Summary:"""
+    example_2_response = f"""{train_data[1]['section_text']}"""
+    examples = {
+        'example_1_question':example_1_question,
+        'example_1_response':example_1_response,
+        'example_2_question':example_2_question,
+        'example_2_response':example_2_response,
+    }
         
     #-------------------
     # load summarizer
@@ -98,21 +77,57 @@ def main():
     #--------------
     model.eval()
     
-    print('Evaluating model on ROUGE, BLEU, and BERTScore...')
-    model_outputs, metrics = evaluate_hf_model(model,
-                                               tokenizer,
+    print('--- ZeroShot Evaluation...')
+    model_outputs, metrics = evaluate_hf_model(model=model,
+                                               tokenizer-tokenizer,
                                                test_data,
-                                               input_column=args.input_column,
-                                               target_column=args.target_column,
+                                               input_column=args.input_col,
+                                               target_column=args.target_col,
                                                max_samples=len(test_data),
-                                               start_prompt=args.start_prompt,
-                                               end_prompt=args.end_prompt,
-                                               remove_suffix=args.suffix)
-    
-    for k, v in metrics.items():
-        print(f'{k}: {v}')
-        
-    np.save(f"{args.model_id.split('/')[1]}_pretrained_model_outputs.npy", model_outputs)
-    
+                                               system_message=system_message,
+                                               transaction=transaction,
+                                               examples = examples,
+                                               remove_suffix=args.suffix,
+                                               shot = 0)
+    print('ZeroShot Results:')
+    for k, v in metrics.items():print(f'{k}: {v}')
+    with open('{args.model_id.split('/')[1]}_pretrained_model_zeroshot_outputs.json', 'w') as f: json.dump(mapping, f)
+    np.save(f"{args.model_id.split('/')[1]}_pretrained_model_zeroshot_outputs.npy", model_outputs)
+
+
+    print('--- OneShot Evaluation...')
+    model_outputs, metrics = evaluate_hf_model(model=model,
+                                               tokenizer-tokenizer,
+                                               test_data,
+                                               input_column=args.input_col,
+                                               target_column=args.target_col,
+                                               max_samples=len(test_data),
+                                               system_message=system_message,
+                                               transaction=transaction,
+                                               examples = examples,
+                                               remove_suffix=args.suffix,
+                                               shot = 1)
+    print('OneShot Results:')
+    for k, v in metrics.items():print(f'{k}: {v}')
+    with open('{args.model_id.split('/')[1]}_pretrained_model_oneshot_outputs.json', 'w') as f: json.dump(mapping, f)
+    np.save(f"{args.model_id.split('/')[1]}_pretrained_model_oneshot_outputs.npy", model_outputs)
+
+    print('--- TwoShot Evaluation...')
+    model_outputs, metrics = evaluate_hf_model(model=model,
+                                               tokenizer-tokenizer,
+                                               test_data,
+                                               input_column=args.input_col,
+                                               target_column=args.target_col,
+                                               max_samples=len(test_data),
+                                               system_message=system_message,
+                                               transaction=transaction,
+                                               examples = examples,
+                                               remove_suffix=args.suffix,
+                                               shot = 2)
+    print('Twoshot Results:')
+    for k, v in metrics.items():print(f'{k}: {v}')
+    with open('{args.model_id.split('/')[1]}_pretrained_model_twoshot_outputs.json', 'w') as f: json.dump(mapping, f)
+    np.save(f"{args.model_id.split('/')[1]}_pretrained_model_twoshot_outputs.npy", model_outputs)
+
 if __name__ == "__main__":
     main()
