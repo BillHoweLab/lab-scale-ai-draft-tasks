a7265b5f5746204527c2fde046b0e320b01f7718
Bin Han <39442036+BeanHam@users.noreply.github.com>
2023-12-14 19:38:53 -0600

    Update evaluate_summarization.py

--- tasks/evaluate_summarization.py
+++ tasks/evaluate_summarization.py
@@ -95,11 +95,11 @@ def main():
     if args.pretrain == 'True':        
         for k, v in metrics.items():print(f'{k}: {v}')
         with open(f"results/{args.model_id.split('/')[1]}_pretrained_model_{args.nshot}shot_outputs.json", 'w') as f: json.dump(metrics, f)
-        np.save(f"{results/args.model_id.split('/')[1]}_pretrained_model_{args.nshot}shot_outputs.npy", model_outputs)
+        np.save(f"results/{args.model_id.split('/')[1]}_pretrained_model_{args.nshot}shot_outputs.npy", model_outputs)
     else:
         for k, v in metrics.items():print(f'{k}: {v}')
         with open(f"results/{args.model_id.split('/')[1]}_finetuned_model_{args.nshot}shot_outputs.json", 'w') as f: json.dump(metrics, f)
-        np.save(f"{results/args.model_id.split('/')[1]}_finetuned_model_{args.nshot}shot_outputs.npy", model_outputs)
+        np.save(f"results/{args.model_id.split('/')[1]}_finetuned_model_{args.nshot}shot_outputs.npy", model_outputs)
 
 if __name__ == "__main__":
     main()
