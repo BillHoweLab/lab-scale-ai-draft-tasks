6dad1c46b24afda55126607721f50a237412f626
Bin Han <39442036+BeanHam@users.noreply.github.com>
2023-12-17 13:24:32 -0800

    Update evaluate_functions.py

--- tasks/evaluate_functions.py
+++ tasks/evaluate_functions.py
@@ -73,8 +73,8 @@ def generate_from_prompt(model: AutoModelForCausalLM,
                          tokenizer: AutoTokenizer, 
                          input_data: str,
                          max_tokens: int=2048,
-                         min_new_tokens: int=25,
-                         max_new_tokens: int=50) -> str:
+                         min_new_tokens: int=1,#25,
+                         max_new_tokens: int=512#50,) -> str:
     """
     Generate and decode output from a Transformers model using a prompt.
     """
@@ -110,8 +110,8 @@ def evaluate_hf_model(model: AutoModelForCausalLM,
                       system_message: str='###',
                       transaction: str='###',
                       max_tokens: int=2048,
-                      min_new_tokens: int=25,
-                      max_new_tokens: int=50,
+                      min_new_tokens: int=1,#25,
+                      max_new_tokens: int=512,#50,
                       remove_suffix: str=None,
                       rouge: bool=True,
                       bleu: bool=True,
