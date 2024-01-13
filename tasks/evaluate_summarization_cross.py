54e2895a5cc275baede9a974f554f38f86760c07
Bin Han <39442036+BeanHam@users.noreply.github.com>
2024-01-12 23:03:04 -0800

    Update evaluate_summarization_cross.py

--- tasks/evaluate_summarization_cross.py
+++ tasks/evaluate_summarization_cross.py
@@ -92,7 +92,7 @@ def main():
     #--------------
     # inference
     #--------------
-    for nshot in ['zero', 'one', 'two', 'three']:
+    for nshot in ['one', 'two', 'three']:
     #for nshot in ['zero']:
         print(f"{nshot}shot...")
         args.nshot = nshot
